11953f9529f95401ef334717dc3a0c3651002739
Aleksandr Sundeev <navigator97@mail.ru>
2024-07-16 12:56:57 +0700

    Add models Users & Materials

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Course(models.Model):
+    """
+    Модель курса.
+    """
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    name = models.CharField(max_length=120, verbose_name="Название куса")
+    image = models.ImageField(
+        upload_to="materials/course/image", **NULLABLE, verbose_name="Изображение"
+    )
+    description = models.TextField(**NULLABLE, verbose_name="Описание курса")
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    """
+    Модель урока.
+    """
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    name = models.CharField(max_length=150, verbose_name="Урок")
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        **NULLABLE,
+        verbose_name="Курс",
+        related_name="lesson",
+    )
+    description = models.TextField(**NULLABLE, verbose_name="Описание урока")
+    image = models.ImageField(
+        upload_to="materials/lesson/image", **NULLABLE, verbose_name="Изображение"
+    )
+    link_to_video = models.CharField(
+        max_length=150, **NULLABLE, verbose_name="Ссылка на видео"
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
--- users/models.py
+++ users/models.py
@@ -4,18 +4,22 @@ from django.db import models
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True, verbose_name='Еmail')
-    phone = models.CharField(max_length=35, verbose_name='Телефон', blank=True, null=True)
+    email = models.EmailField(unique=True, verbose_name="Еmail")
+    phone = models.CharField(
+        max_length=35, verbose_name="Телефон", blank=True, null=True
+    )
     city = models.CharField(max_length=35, verbose_name="Город", blank=True, null=True)
-    avatar = models.ImageField(upload_to='users/avatars/', verbose_name='Аватар', blank=True, null=True)
+    avatar = models.ImageField(
+        upload_to="users/avatars", verbose_name="Аватар", blank=True, null=True
+    )
     # token = models.CharField(max_length=100, verbose_name='Токен', blank=True, null=True)
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
 
     def __str__(self):
         return self.email
