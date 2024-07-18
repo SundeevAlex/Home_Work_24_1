ae95134f7467f631a17c211b953cb932d500ee88
Aleksandr Sundeev <navigator97@mail.ru>
2024-07-18 16:40:21 +0700

    Add model Payments

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from materials.models import Course, Lesson
 
 
 class User(AbstractUser):
@@ -23,3 +24,24 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payments(models.Model):
+    METHOD_CHOISES = [
+        ('CASH', 'Наличные'),
+        ('TRAN', 'Перевод')
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    date_of_pay = models.DateField(auto_now=True, verbose_name='Дата оплаты')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Оплаченный курс', blank=True, null=True)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='Оплаченный урок', blank=True, null=True)
+    amount = models.PositiveIntegerField(verbose_name='Сумма оплаты')
+    method = models.CharField(max_length=4, choices=METHOD_CHOISES)
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
+
+    def __str__(self):
+        return self.user
