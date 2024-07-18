c5c790e45735bc66528abc2d637193038cb1d1cf
Aleksandr Sundeev <navigator97@mail.ru>
2024-07-18 15:56:47 +0700

    Fixed & add task 3

--- materials/serializers.py
+++ materials/serializers.py
@@ -11,19 +11,32 @@ class CourseSerializer(ModelSerializer):
         fields = "__all__"
 
 
-class CourseDetailSerializer(ModelSerializer):
-    count_lesson_in_course = SerializerMethodField()
-
-    def get_count_lesson_in_course(self, course):
-        return Lesson.objects.filter(course=course).count()
-
-    class Meta:
-        model = Course
-        fields = ('name', 'description', 'image', 'count_lesson_in_course',)
+# class CourseDetailSerializer(ModelSerializer):
+#     count_lesson_in_course = SerializerMethodField()
+#
+#     def get_count_lesson_in_course(self, course):
+#         return Lesson.objects.filter(course=course).count()
+#
+#     class Meta:
+#         model = Course
+#         fields = ('name', 'description', 'image', 'count_lesson_in_course',)
 
 
 class LessonSerializer(ModelSerializer):
+    course = CourseSerializer()
 
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class LessonDetailSerializer(ModelSerializer):
+    count_lesson_in_course = SerializerMethodField()
+    course = CourseSerializer()
+
+    def get_count_lesson_in_course(self, lesson):
+        return Lesson.objects.filter(course=lesson.course).count()
+
+    class Meta:
+        model = Lesson
+        fields = ('name', 'course', 'description', 'count_lesson_in_course',)
--- materials/urls.py
+++ materials/urls.py
@@ -2,19 +2,20 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from materials.apps import MaterialsConfig
-from materials.views import (CourseViewSet, LessonCreateAPIView,
+from materials.views import (LessonViewSet, LessonCreateAPIView,
                              LessonDestroyAPIView, LessonListAPIView,
-                             LessonRetrieveAPIView, LessonUpdateAPIView)
+                             LessonRetrieveAPIView, LessonUpdateAPIView, CourseListAPIView, CourseRetrieveAPIView,
+                             CourseCreateAPIView, CourseDestroyAPIView, CourseUpdateAPIView)
 
 app_name = MaterialsConfig.name
 
 router = DefaultRouter()
-router.register(r'', CourseViewSet, basename='')
+router.register(r'', LessonViewSet)
 
 urlpatterns = [
-    path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
-    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_retrieve'),
-    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
-    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
-    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
-] + router.urls
+                  path('courses/', CourseListAPIView.as_view(), name='courses_list'),
+                  path('courses/<int:pk>/', CourseRetrieveAPIView.as_view(), name='courses_retrieve'),
+                  path('courses/create/', CourseCreateAPIView.as_view(), name='courses_create'),
+                  path('courses/delete/<int:pk>/', CourseDestroyAPIView.as_view(), name='courses_delete'),
+                  path('courses/update/<int:pk>/', CourseUpdateAPIView.as_view(), name='courses_update'),
+              ] + router.urls
