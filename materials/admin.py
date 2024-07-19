5af14dec6db6911dde6f6ee66d8cf57650388811
Aleksandr Sundeev <navigator97@mail.ru>
2024-07-19 23:27:56 +0700

    Fixed admin

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Lesson, Course
 
-# Register your models here.
+
+@admin.register(Lesson)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'course', 'description', 'image', 'link_to_video',)
+    search_fields = ('name',)
+    list_filter = ('name', 'course', )
+
+
+@admin.register(Course)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image', 'description',)
+    search_fields = ('name',)
+    list_filter = ('name',)
--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
 from users.models import User
+from .models import Payments
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     pass
 
+
+@admin.register(Payments)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('date_of_pay', 'course', 'lesson', 'amount', 'method',)
+    search_fields = ('date_of_pay',)
+    list_filter = ('date_of_pay', 'course', 'lesson', 'method', )
