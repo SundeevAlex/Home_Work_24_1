66208716084648759d6cc9b6d6a3b0fad7d8694b
Aleksandr Sundeev <navigator97@mail.ru>
2024-08-02 13:19:02 +0700

    Add mail sending

--- materials/tasks.py
+++ materials/tasks.py
@@ -16,17 +16,14 @@ def send_notification(pk):
     course = Course.objects.get(pk=pk)
     subscription = Subscribe.objects.filter(course_id=pk, sign_of_subscription=True)
     users_id_list = []
-    print('1AAAAAAAAAAAAAAAAAAAAAAAAA')
     if subscription:
         for el in subscription:
             users_id_list.append(el.user_id)
         if len(users_id_list) > 0:
             user_email = []
-            print('2AAAAAAAAAAAAAAAAAAAAAAAAA', user_email)
             for item in users_id_list:
                 email = User.objects.get(pk=item).email
                 user_email.append(email)
-                print('3AAAAAAAAAAAAAAAAAAAAAAAAA', user_email)
                 send_mail(
                     subject=f"Внимание, обновление!",
                     message=f"В курсе '{course.title}' произошли изменения.",
